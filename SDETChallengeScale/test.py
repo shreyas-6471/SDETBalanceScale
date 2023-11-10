ac617d21e75bd4e5988f67f9dfdf8124d93333ec
shreyas-6471 <37994152+shreyas-6471@users.noreply.github.com>
2023-11-10 09:36:28 -0500

    Update test.py

--- SDETChallengeScale/test.py
+++ SDETChallengeScale/test.py
@@ -3,60 +3,72 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-driver = webdriver.Firefox()
-driver.get("http://sdetchallenge.fetch.com/")
-counterfeit_bar = float('inf')
 counter=1
-def fill_board(side, values):
-    time.sleep(5)
-    xpath = "(//div[contains(@class, 'board-row')])[" + str(side) + "]" + "//input"
+def setup_driver():
+    """Sets up the Selenium WebDriver."""
+    driver = webdriver.Firefox()
+    driver.get("http://sdetchallenge.fetch.com/")
+    return driver
+
+def fill_board(driver, side, values):
+    """Fills the specified side of the board with given values."""
+    xpath = f"(//div[contains(@class, 'board-row')])[{side}]//input"
     inputs = WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located((By.XPATH, xpath))
     )
     for i, value in enumerate(values):
         inputs[i].send_keys(str(value))
-def weigh_bars(left_start, left_end, right_start, right_end):
+        
+def weigh_bars(driver, left_values, right_values):
+    """Performs the weighing of bars and returns the result."""
     driver.find_element(By.XPATH, "//button[text()='Reset']").click()
-    left_values = []
-    right_values = []
-    for i in range(left_start, left_end + 1):
-        left_values.append(i)
-    for i in range(right_start, right_end + 1):
-        right_values.append(i)
-    fill_board(1, left_values)
-    fill_board(4, right_values)
-    driver.find_element(By.XPATH,"//button[text()='Weigh']").click()
+    fill_board(driver, 1, left_values)
+    fill_board(driver, 4, right_values)
+    driver.find_element(By.XPATH, "//button[text()='Weigh']").click()
     global counter
-    wait = WebDriverWait(driver, 20)
-    elements = wait.until(lambda driver: len(driver.find_elements(By.XPATH, "//li")) >= counter)
-    time.sleep(5)
+    WebDriverWait(driver, 20).until(
+        lambda d: len(d.find_elements(By.XPATH, "//li")) >= (counter)
+    )
+    counter=counter+1
     last_li_element = driver.find_elements(By.XPATH, "//li")[-1]
     return last_li_element.text
-def perform_search(start, end):
-    global counterfeit_bar 
-    time.sleep(5)
-    if (start == end):
-        buttonxpth="//button[text()='"+str(start)+"']"
-        driver.find_element(By.XPATH,buttonxpth).click()
-        counterfeit_bar = start  
+
+def perform_search(driver, start, end):
+    """Recursively searches for the counterfeit bar and returns its number."""
+    if start == end:
+        select_fake_bar(driver, start)
         return start
-    numBars = end - start + 1
-    third = numBars // 3
-    operatorexpression = weigh_bars(start, start + third - 1, start + third, start + 2 * third - 1)
-    if "=" in operatorexpression:
-        perform_search(start + 2 * third, end)
-    elif "<" in operatorexpression:
-        perform_search(start, start + third - 1)
+
+    num_bars = end - start + 1
+    third = num_bars // 3
+    result = weigh_bars(driver, list(range(start, start + third)),
+                        list(range(start + third, start + 2 * third)))
+    print("Result is",result)
+
+    if "=" in result:
+        return perform_search(driver, start + 2 * third, end)
+    elif "<" in result:
+        return perform_search(driver, start, start + third - 1)
     else:
-        perform_search(start + third, start + 2 * third - 1)
-perform_search(0, 8)
-time.sleep(5)
-WebDriverWait(driver, 10).until(EC.alert_is_present())
-alert = driver.switch_to.alert
-alert.accept()
-time.sleep(10)
-driver.quit()
+        return perform_search(driver, start + third, start + 2 * third - 1)
+
+def select_fake_bar(driver, bar_number):
+    """Selects the fake bar and handles the alert."""
+    button_xpath = f"//button[text()='{bar_number}']"
+    #print("Xpath is",button_xpath)
+    time.sleep(10)
+    driver.find_element(By.XPATH, button_xpath).click()
+    time.sleep(10) 
+    WebDriverWait(driver, 10).until(EC.alert_is_present())
+    alert = driver.switch_to.alert
+    alert.accept()
+
+def main():
+    """Main function to execute the counterfeit bar finding process."""
+    driver = setup_driver()
+    fake_bar_number = perform_search(driver, 0, 8)
+    print(f"Fake bar is number: {fake_bar_number}")
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
